7d28c52f51cd4e0beba85b90ee4c4cca7ba7c3e0
gitaugust <nithyamanju15.6.2000@gmail.com>
2024-08-15 22:40:51 +0530

    my task done

---
+++ src/gitjune/gitdone.java
@@ -0,0 +1,14 @@
+package gitjune;
+
+public class gitdone {
+ private void empid() {
+	 System.out.println("bct");
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+gitdone g = new gitdone();
+g.empid();
+	}
+
+}
